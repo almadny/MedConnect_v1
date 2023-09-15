f2dad8dcd844d109c743af2b2b1f7e6fddccfced
Almadny <Almadny2u@gmail.com>
2023-09-15 13:23:13 +0000

    Added error routes

--- backend/MedConnect/api/index.py
+++ backend/MedConnect/api/index.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, request
 
 main = Blueprint('main', __name__)
 
 @main.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """ First JSON file """
-    return jsonify({'status': 'Ok'}), 200
+    return jsonify({'Status': 'Ok'}), 200
