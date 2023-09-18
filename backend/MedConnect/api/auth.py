0ef3096e217c496b1e57996c78237706d9caa8f0
Almadny <Almadny2u@gmail.com>
2023-09-18 20:55:28 +0000

    Refactor Authentication and other routes

--- backend/MedConnect/api/auth.py
+++ backend/MedConnect/api/auth.py
@@ -7,14 +7,18 @@ auth_bp = Blueprint('auth_bp', __name__)
 
 @auth_bp.route("/login", methods=["POST"])
 def login():
-    data = request.get_json()
-    username = data.get("username")
-    password = data.get("password")
+    try:
+        data = request.get_json()
+        email = data.get("email_address")
+        password = data.get("password")
 
-    """ Check if the user exists in the database"""
-    user = is_user(email)
-    if user and check_password_hash(user.hashed_password, password):
-        access_token = create_access_token(identity=user.id)
-        return jsonify({"access_token": access_token}), 200
-        # 401 Unauthorized
-    return jsonify({"message": "Invalid username or password"}), 401
+        """ Check if the user exists in the database"""
+        from api.main import is_user
+        user = is_user(email)
+        if user and check_password_hash(user.hashed_password, password):
+            access_token = create_access_token(identity=user.id)
+            return jsonify({"access_token": access_token}), 200
+            # 401 Unauthorized
+        return jsonify({"message": "Invalid username or password"}), 401
+    except Exception as e:
+        print(e)
